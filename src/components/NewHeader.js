46980340ba1932f2e2b7e0eff23a18a0fb518f75
raju <rajudampoori@gmail.com>
2023-01-02 22:50:57 +0530

    fourth  commit

--- src/components/NewHeader.js
+++ src/components/NewHeader.js
@@ -11,7 +11,7 @@ const NewHeader = () => {
   const logoutuser = async () => {
     let token = localStorage.getItem("usersdatatoken");
 
-    const res = await fetch("./logout", {
+    const res = await fetch("https://realestatenewbackend.onrender.com/logout", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
